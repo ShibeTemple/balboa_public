67406d51905eabc33b88efbe24b0dc09fc0ea541
ShibeTemple <thelocken@gmail.com>
2023-11-20 17:06:58 -0800

    HW3_2 completed (rotation)

--- src/hw3.cpp
+++ src/hw3.cpp
@@ -5,6 +5,26 @@
 
 using namespace hw3;
 
+// GLSL
+// rotation too
+const char* vertexShaderSource = "#version 330 core\n" 
+    "layout (location = 0) in vec3 aPos;\n"
+    "uniform float rotationAngle;\n" // single precision warning. (GLSL:1,balboa:2)
+    "void main()\n"
+    "{\n"
+    "   mat2 rotationMatrix = mat2(cos(rotationAngle), -sin(rotationAngle),\n"
+    "                           sin(rotationAngle), cos(rotationAngle));\n"
+    "   vec2 rotatedPosition = rotationMatrix * aPos.xy;\n"
+    //"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = vec4(rotatedPosition, aPos.z, 1.0);\n"
+    "}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -25,7 +45,9 @@ void hw_3_1(const std::vector<std::string> &params) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); mac os 
+    #ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //mac os 
+    #endif
 
     // Create Application Window
     GLFWwindow* window = glfwCreateWindow(800, 600, "CSE 167 HW3", NULL, NULL);
@@ -70,8 +92,167 @@ void hw_3_1(const std::vector<std::string> &params) {
     return;
 }
 
+
 void hw_3_2(const std::vector<std::string> &params) {
     // HW 3.2: Render a single 2D triangle
+
+    // GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);    
+    #ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //mac os 
+    #endif
+
+    // Create Application Window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "CSE 167 HW3", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return;
+    }
+    glfwMakeContextCurrent(window);
+
+    // GLAD: OpenGL function pointers (OS specific)
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return;
+    }
+
+    // Create Application Window Viewport Size
+    glViewport(0, 0, 800, 600);
+
+    // Define Application Window Resize Callback (gets run on launch as well)
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int VBO; // vertex buffer object
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //GL_STREAM_DRAW : the data is set only once and used by the GPU at most a few times.
+    //GL_STATIC_DRAW : the data is set only once and used many times.
+    //GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+
+    // SHADERS:
+    // --------
+    // define vertex shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // attach source code to object and compile
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); // object, # src lines, src.
+    glCompileShader(vertexShader);
+    // check for success
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // define fragment shader 
+    // one output,vector of size 4, defines the final color output 
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Shader program: combine all shaders
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glUseProgram(shaderProgram);
+    // cleanup individual shader objects as they are now in shader program
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+
+    // Get the uniform location
+    GLint rotationAngleLocation = glGetUniformLocation(shaderProgram, "rotationAngle");
+
+
+    // render loop
+    // -----------
+    // An iteration of the render loop is more commonly called a frame
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window); // user input
+
+        // rendering commands here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // state-setting function
+        glClear(GL_COLOR_BUFFER_BIT); // state-using function
+
+        // Update rotation angle (adjust the factor for the speed of rotation)
+        float rotationAngle = static_cast<float>(glfwGetTime()) * 0.5f;
+        
+
+        // 0 draw our first triangle
+        glUseProgram(shaderProgram);
+        // Set the rotation angle uniform
+        glUniform1f(rotationAngleLocation, rotationAngle);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glBindVertexArray(0); // no need to unbind it every time 
+
+        // check and call events and swap the buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    // cleanup
+    glfwTerminate();
+    return;
 }
 
 void hw_3_3(const std::vector<std::string> &params) {
